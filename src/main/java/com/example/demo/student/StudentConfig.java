3745792fabf4ab86da78d2bf2d4ad647c82738d1
JAVACspb <al.alentjev@yandex.ru>
2022-10-17 18:28:39 +0300

    test

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -11,6 +11,7 @@ import java.util.List;
 @Configuration
 public class StudentConfig {
 
+
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository repository){
         return args -> {
